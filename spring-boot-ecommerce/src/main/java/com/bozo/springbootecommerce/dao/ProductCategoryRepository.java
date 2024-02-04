d4e24f1cbe9ff5a9053ecca1ca48d4f0bbda89ad
Mateusz Gulczewski <bozo1209@gmail.com>
2024-02-04 19:01:43 +0100

    make custom configuration rest apis #5

--- spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/dao/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.bozo.springbootecommerce.dao;
 
 import com.bozo.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
