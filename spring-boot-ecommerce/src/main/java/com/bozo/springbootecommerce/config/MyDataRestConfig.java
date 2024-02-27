bc63f59f2dfe72474d937c2d63fa81ce95b3dcfa
Mateusz Gulczewski <bozo1209@gmail.com>
2024-02-27 23:13:11 +0100

    modify spring boot app - expose entity ids #45

--- spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/config/MyDataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,25 @@ package com.bozo.springbootecommerce.config;
 
 import com.bozo.springbootecommerce.entity.Product;
 import com.bozo.springbootecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.Type;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -28,5 +39,19 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metaData, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metaData, httpMethods) -> httpMethods.disable(unsupportedActions));
 
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        config.exposeIdsFor(
+                        entityManager
+                                .getMetamodel()
+                                .getEntities()
+                                .stream()
+                                .map(Type::getJavaType)
+                                .toList()
+                                .toArray(new Class[0])
+                );
     }
 }
