84694c4fe18ee999d1e11a20510d2fe476e5f897
Mateusz Gulczewski <bozo1209@gmail.com>
2024-03-03 00:16:19 +0100

    add support for case insensitive search #47

--- spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/bozo/springbootecommerce/dao/ProductRepository.java
@@ -12,6 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
-    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+    Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable page);
 
 }
